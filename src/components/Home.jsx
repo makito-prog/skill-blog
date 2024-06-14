88a8caf21f4f6583af373ffc25dd702524bc595b
makito-prog <makito.soccer.28220305@gmail.com>
2024-06-14 14:16:07 +0900

    Homeコンポーネントを追加

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Header from './Header'
+// src/components/Home.js
+import React from 'react';
+import Header from './Header';
 
 const Home = () => {
   return (
-    <>
-        <Header />
-        <div>Skill Blog</div>
-    </>
-    
-  )
-}
+    <div>
+      <Header />
+      <h1 className='home-title'>Welcome to My Tech Blog</h1>
+      <p className='home-content'>This is a blog about the latest in tech, programming tutorials, and more!</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
